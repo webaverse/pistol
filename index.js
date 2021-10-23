2226d9e1f90869cab5588606e451490e412fb136
Avaer Kazmer <a@modules.io>
2021-10-23 03:02:02 -0400

    Add pistol subtype

--- index.js
+++ index.js
@@ -86,7 +86,9 @@ export default () => {
         },
         {
           "key": "use",
-          "value": {}
+          "value": {
+            "subtype": "pistol"
+          }
         }
       ];
       
