c565f7162c011b8178ef11d87a67f1835db3236d
Ejaz <ejaz345@hotmail.com>
2021-11-22 18:37:21 +0000

    debugging

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ export default () => {
 
                       const convertedVal = new Float32Array(vertexRaycast.point)
                       const pointVec = new THREE.Vector3().fromArray(convertedVal);
-                      planeGeo.attributes.position.setXYZ( i, 0.25, p.y, p.z );
+                      planeGeo.attributes.position.setXYZ( i, p.x + i, p.y+ i, p.z+ i );
 
                       if(i < planeNewVertices.length - 1) {
 
