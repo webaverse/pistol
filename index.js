d3d08618288502f2b38dbd9896cb1840f5e5e4ff
Ejaz <ejaz345@hotmail.com>
2021-11-22 00:21:19 +0000

    drawing debug lines

--- index.js
+++ index.js
@@ -237,14 +237,14 @@ export default () => {
                         color: 0x0000ff
                       });
                       
-                      const points = [];
-                      points.push( convertedVal );
-                      points.push( convertedVal.multiply(2) );
+                      // const points = [];
+                      // points.push( convertedVal );
+                      // points.push( convertedVal.multiply(2) );
                       
-                      const geometry = new THREE.BufferGeometry().setFromPoints( points );
+                      // const geometry = new THREE.BufferGeometry().setFromPoints( points );
                       
-                      const line = new THREE.Line( geometry, material );
-                      scene.add( line );
+                      // const line = new THREE.Line( geometry, material );
+                      // scene.add( line );
 
 
                       if(i < planeNewVertices.length - 1) {
