ac9193a8d5d6680ab6b950f553942ddfd076c745
Ejaz <ejaz345@hotmail.com>
2021-11-22 22:52:51 +0000

    Applying offset

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ export default () => {
                       debugCube.position.set(pointVec.x, pointVec.y, pointVec.z);
                       debugCube.updateWorldMatrix();
                       const worldToLoc = plane.worldToLocal(pointVec)
-                      const offset = worldToLoc.add(new Vector3(vertextHitnormal.y, 0,0));
+                      const offset = worldToLoc.add(new Vector3(0, 0,vertextHitnormal.y));
                       planeGeo.attributes.position.setXYZ( i, offset.x , offset.y, offset.z );
                      
 
