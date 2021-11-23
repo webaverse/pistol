99c68191624e87a606672a69c6f54335edeac32d
Ejaz <ejaz345@hotmail.com>
2021-11-23 01:17:16 +0000

    increasing plane segments

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ export default () => {
                         // const debugCube = new THREE.Mesh( debugGeo, debugMat );
                         // scene.add( debugCube );
                         const convertedVal = new Float32Array(vertexRaycast.point)
-                        const pointVec =  debugCube.localToWorld(new THREE.Vector3().fromArray(convertedVal));
+                        const pointVec =  plane.localToWorld(new THREE.Vector3().fromArray(convertedVal));
                         //debugCube.position.set(pointVec.x, pointVec.y, pointVec.z);
                         //debugCube.updateWorldMatrix();
                         const worldToLoc = plane.worldToLocal(pointVec)
