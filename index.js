201d6b00b70a215bfbc72b494107eb9787429bdb
Ejaz <ejaz345@hotmail.com>
2021-11-25 22:53:24 +0000

    logs

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ export default () => {
                         console.log("Plane world position", plane)
                         console.log("Hit position local: worldToLoc",worldToLoc);
 
-                        const clampedPos = new Vector3(clamp(worldToLoc.x, 0.0, 1), clamp(worldToLoc.y, 0.0, 1), clamp(worldToLoc.z, 0.0, 1));
+                        const clampedPos = new Vector3(clamp(worldToLoc.x, -0.001, 3), clamp(worldToLoc.y, -0.001, 3), clamp(worldToLoc.z, -0.001, 3));
                         //const offset = worldToLoc.add(new Vector3(vertextHitnormal.x / 20, vertextHitnormal.y / 20,vertextHitnormal.z / 20));
                         planeGeo.attributes.position.setXYZ( i, clampedPos.x, clampedPos.y, clampedPos.z );
                       }
