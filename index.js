1b88663f4b501658990f1568ccec389cdaa6bf45
aerydna <ruzzenenti.a@gmail.com>
2022-12-20 01:55:07 +0100

    Revert "updates sound asset"
    This reverts commit 3379a929c5b304db7d8b0079e9fc58d2acb1eede.

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ export default e => {
     app.matrixWorld.copy(subApp.matrixWorld);
   }; */
   const sounds = useSound();
+  const soundFiles = sounds.getSoundFiles();
+  const soundIndex=soundFiles.combat.map(sound => sound.name).indexOf('combat/Colt45_Shot2.wav');
+  
+  
   let pointLights = [];
   const gunPointLight = new THREE.PointLight(0xFFFFFF, 5);
   gunPointLight.castShadow = false; 
@@ -483,7 +487,7 @@ export default e => {
   
   useUse(e => {
     if (e.use && gunApp) {
-      sounds.playSound("pistol_single");
+      sounds.playSound(soundFiles.combat[soundIndex]);
       gunApp.use();
     }
   });
